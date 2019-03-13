1cad376b7f3275b3c79403417084c700505f3893
shlee <shlee@shlees-MacBook-Pro.local>
2019-03-13 14:08:48 +0900

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -83,7 +83,7 @@ define([
 
     function save() {
         var entryDefinitionkey = "DEAudience-8ed7573a-3ce5-c9f6-50e0-75cf778db7f9";
-        var message_body = "(광고)가 아닌듯 맘큐(momQ) 알림!!\n\n%%col1%% 님 ♬~♪~\n지난 번에 구매했던 그린핑거 기억나시나요?\n\n구매 시 맘큐가 약속 드렸던,\n맘큐만의 재구매 할인 프로그램 \n♪ REMEMBER 알림입니다!\n\n\" %%col2%% 님의\n[REMEMBER] 할인 쿠폰 지급 완료! \"\n\n▤ 내 쿠폰함 바로가기 \nhttp://bit.ly/2Jwj6xu\n※오늘부터 14일내에 사용 가능합니다.\n\n그린핑거 제품을 다시 구매하신다면 꼭!! 할인 받아 구매하시고, [REMEMBER] 할인 쿠폰 으로 계속 계속 할인 받아 쭈~욱 구매하세요! \n\n다음 리멤버 알림에서\n또! 찾아뵙겠습니다~ ^^*\n\n맘큐만의 REMEMBER 는 계속됩니다.\nI'll be back ***\n\n맘큐(momQ) : http://m.momq.co.kr\n무료수신거부 : 080-884-7933".replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r");
+        var message_body = "(광고)가 아닌듯 맘큐(momQ) 알림!!\n\n%%col1%% 님 ♬~♪~\n지난 번에 구매했던 그린핑거 기억나시나요?\n\n구매 시 맘큐가 약속 드렸던,\n맘큐만의 재구매 할인 프로그램 \n♪ REMEMBER 알림입니다!\n\n\" %%col2%% 님의\n[REMEMBER] 할인 쿠폰 지급 완료! \"\n\n▤ 내 쿠폰함 바로가기 \nhttp://bit.ly/2Jwj6xu\n※오늘부터 14일내에 사용 가능합니다.\n\n그린핑거 제품을 다시 구매하신다면 꼭!! 할인 받아 구매하시고, [REMEMBER] 할인 쿠폰 으로 계속 계속 할인 받아 쭈~욱 구매하세요! \n\n다음 리멤버 알림에서\n또! 찾아뵙겠습니다~ ^^*\n\n맘큐만의 REMEMBER 는 계속됩니다.\nI'll be back ***\n\n맘큐(momQ) : http://m.momq.co.kr\n무료수신거부 : 080-884-7933".replace(/\n/g, "\\n").replace(/\r/g, "\\r");
         
         
         console.log(JSON.stringify(payload['arguments'].execute.inArguments));
